784fcbf586c9877b9783a6d8e488e336f79e6fb6
chronoguy <chronoguy@temporal.io>
2021-12-15 21:19:52 -0800

    Add Workflow-related attribute discovery PoC.

---
+++ src/InternalAlgorithms/ReflectionAnalysisPoC/Attributes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionAnalysisPoC
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, Inherited = true, AllowMultiple = true)]
+    public class WorkflowAttribute : Attribute
+    {
+        public string RunMethod { get; }
+
+        public WorkflowAttribute()
+            : this(String.Empty)
+        {
+        }
+
+        public WorkflowAttribute(string runMethod)
+        {
+            RunMethod = runMethod;
+        }
+
+        public override bool IsDefaultAttribute()
+        {
+            return false;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class WorkflowSignalAttribute : Attribute
+    {
+        public string SignalTypeName { get; }
+
+        public WorkflowSignalAttribute()
+            : this(String.Empty)
+        {
+        }
+
+        public WorkflowSignalAttribute(string signalTypeName)
+        {
+            SignalTypeName = signalTypeName;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class WorkflowQueryAttribute : Attribute
+    {
+        public string QueryTypeName { get; }
+
+        public WorkflowQueryAttribute()
+            : this(String.Empty)
+        {
+        }
+
+        public WorkflowQueryAttribute(string queryTypeName)
+        {
+            QueryTypeName = queryTypeName;
+        }
+    }
+}
